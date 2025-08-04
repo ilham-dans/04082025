1cb286f864b935d56db8a1131285283d72688c2b
ilham-dans <muhammad_daniswara_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:16:51 +0700

    Add files via upload

--- Kelas.java
+++ Kelas.java
@@ -1,5 +1,12 @@
 public class Kelas {
     public static void main(String[] args) {
+        //main method
         System.out.println("Ilham");    
+        System.out.println("SMK Telkom Malang");
+        System.out.println("MtsN 1 kota Malang");
+        System.out.println("Hanafi");
+        System.out.println("Ana");
+        System.out.println("software engineer dan sukses di bidang IT");
+        System.out.println("Futsal dan Trail Run");
 }
 }
