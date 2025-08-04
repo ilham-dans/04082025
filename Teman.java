f2eb5ec93afc42901bff0ca3105e2f2cfa4d540b
ilham-dans <muhammad_daniswara_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:37:34 +0700

    Add files via upload

---
+++ Teman.java
@@ -0,0 +1,46 @@
+public class Teman {
+    
+    public static void main(String[] args) {
+        System.out.println("Teman 1");
+String nama = "ramadhani putra";
+int usia = 15;
+String cita = "bekerja di jepang";
+System.out.println("Nama:" + nama);
+System.out.println("Usia: " + usia);
+System.out.println("cita: " + cita + "\n");
+
+System.out.println("teman 2");
+String nama1 = "Maven Helios ";
+int usia1 = 15;
+String cita1 = "web developer";
+System.out.println("Nama: " + nama1);
+System.out.println("Usia: " + usia1);
+System.out.println("Hobi: " + cita1+ "\n");
+
+System.out.println("teman 3");
+String nama2 = "Iqbal ibrahim";
+int usia2 = 15;
+String cita2 = "bussinessman";
+System.out.println("Nama: " + nama2);
+System.out.println("Usia: " + usia2);
+System.out.println("Cita: " + cita2+ "\n");
+
+System.out.println("teman 4");
+String nama3 = "Hisyam darmawan";
+int usia3 = 14;
+String cita3 = "pembalap";
+System.out.println("Nama: " + nama3);
+System.out.println("Usia: " + usia3);
+System.out.println("Cita: " + cita3+ "\n");
+
+System.out.println("teman 5");
+String nama4 = "Baihaqi jabbar zeinuar";
+int usia4 = 15;
+String cita4 = "Saldo 2 digit";
+System.out.println("Nama: " + nama4);
+System.out.println("Usia: " + usia4);
+System.out.println("Cita: " + cita4+ "\n");
+
+
+}
+}
